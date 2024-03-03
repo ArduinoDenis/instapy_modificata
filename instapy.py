b00bea1b60f1e211c7cbe8af36748c95f992080e
arduinodenis <90153974+ArduinoDenis@users.noreply.github.com>
2024-03-03 19:26:45 +0100

    Update instapy.py

--- instapy.py
+++ instapy.py
@@ -2,11 +2,11 @@ import random
 from instapy import InstaPy
 from instapy import smart_run
 
-# login credentials
-insta_username = ''
-insta_password = ''
+# Login credentials
+insta_username = ''  # Insert your Instagram username
+insta_password = ''  # Insert your Instagram password
 
-#HASHTAG CHE SE PRESENTI IMPEDIRANNO LE INTERAZIONI CON LE FOTO
+# Hashtags that will prevent interactions with photos
 dont_likes = ['sex', 'nude', 'naked', 'beef', 'pork', 'seafood',
               'egg', 'chicken', 'cheese', 'sausage', 'lobster',
               'fisch', 'schwein', 'lamm', 'rind', 'kuh', 'meeresfrüchte',
@@ -19,45 +19,46 @@ dont_likes = ['sex', 'nude', 'naked', 'beef', 'pork', 'seafood',
               'cream', 'hunt', 'gun', 'shoot', 'slaughter', 'pussy',
               'breakfast', 'dinner', 'lunch']
 
+# List of friends to avoid interaction with
 friends = ['list of friends I do not want to interact with']
 
-#SOSTITUISCI GLI HASHTAG SOTTO CON QUELLI RELATIVI AI TUOI CONTENUTI
-like_tag_list = ['travel','trip','journey','beach','city','travelphotography','photography','travelgram','nature','wanderlust','photooftheday','instatravel','love','instagood','adventure','explore','sydney','ocean','travelblogger','picoftheday','seeaustralia','sunset','traveling','landscape','holiday','summer']
+# Hashtags related to your content
+like_tag_list = ['travel', 'trip', 'journey', 'beach', 'city', 'travelphotography',
+                 'photography', 'travelgram', 'nature', 'wanderlust', 'photooftheday',
+                 'instatravel', 'love', 'instagood', 'adventure', 'explore', 'sydney',
+                 'ocean', 'traveling', 'landscape', 'holiday', 'summer']
 
-# prevent posts that contain some plantbased meat from being skipped
-ignore_list = []
+# Users with similar content
+accounts = ['USER1', 'USER2', 'USER3', 'USER4', 'USER5', 'USER6']  # Replace with users similar to yours
 
-accounts = ['USER1', 'USER2', 'USER3','USER4', 'USER5', 'USER6'] # SOSTITUISCI USER1,2,3,4 CON UTENTI CHE HANNO PAGINE SIMILI ALLA TUA
-
-# get a session!
+# Get a session
 session = InstaPy(username=insta_username,
                   password=insta_password,
                   headless_browser=False,
-		  want_check_browser=False)
+                  want_check_browser=False)
 
 with smart_run(session):
-    # settings
+    # Settings
     session.set_relationship_bounds(enabled=True,
                                     max_followers=15000)
-
     session.set_dont_include(friends)
     session.set_dont_like(dont_likes)
     session.set_ignore_if_contains(ignore_list)
 
-    #sleep rules !important
+    # Sleep rules
     session.set_action_delays(enabled=True,
-                           like=30,
-                           comment=29,
-                           follow=71,
-                           unfollow=28,
-                           story=10,
-                           randomize=True, random_range_from=70, random_range_to=140)
+                              like=30,
+                              comment=29,
+                              follow=71,
+                              unfollow=28,
+                              story=10,
+                              randomize=True, random_range_from=70, random_range_to=140)
 
     session.set_user_interact(amount=2, randomize=True, percentage=60)
     session.set_do_follow(enabled=True, percentage=40)
     session.set_do_like(enabled=True, percentage=80)
 
-    # activity
+    # Activity
     session.like_by_tags(random.sample(like_tag_list, 3),
                          amount=random.randint(50, 100), interact=True)
 
@@ -68,16 +69,16 @@ with smart_run(session):
     """ Joining Engagement Pods...
     """
     photo_comments = ['Nice photo! @{}',
-        'I really like your profile! @{}',
-        'Really great content! :thumbsup:',
-        'Just incredible :open_mouth:',
-        'What trick did you use to frame the shot @{}?',
-        'Love your posts @{}',
-        'This looks awesome @{}',
-        'Getting inspired by you. Kepp up the good work! @{}',
-        ':raised_hands: Yes!',
-        'I can feel your passion for real! @{} :muscle:']
+                      'I really like your profile! @{}',
+                      'Really great content! :thumbsup:',
+                      'Just incredible :open_mouth:',
+                      'What trick did you use to frame the shot @{}?',
+                      'Love your posts @{}',
+                      'This looks awesome @{}',
+                      'Getting inspired by you. Keep up the good work! @{}',
+                      ':raised_hands: Yes!',
+                      'I can feel your passion for real! @{} :muscle:']
 
-    session.set_do_comment(enabled = True, percentage = 95)
-    session.set_comments(photo_comments, media = 'Photo')
+    session.set_do_comment(enabled=True, percentage=95)
+    session.set_comments(photo_comments, media='Photo')
     session.join_pods(topic='travel')
